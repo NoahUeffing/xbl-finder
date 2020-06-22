988b3b62830797365950f9a3ce19ff29d0a29c4e
Noah Ueffing <noahueffing@gmail.com>
2020-06-22 15:55:38 -0300

    added about page and router

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+export const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to search your Xbox Live Friends.</p>
+    </Fragment>
+  );
+};
+
+export default About;
